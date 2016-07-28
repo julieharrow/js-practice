78a2205cf9f61145ad56a459857f1c24f0fb99fe
Juile Harrow <julie.ann.harrow@gmail.com>
2016-07-28 08:46:19 -0400

    initial commit

---
+++ lesson_challenges/lesson01.js
@@ -0,0 +1,8 @@
+//Challenge 1 - Do you need more coffee?
+var numCups = prompt("How many cups of coffee have you had today?");
+if (numCups < 3) {
+  console.log("Yes I'll take another cup of coffee");
+}
+else {
+  console.log("I think I'm okay for now.");
+}
