f70c39f347581951a24e5f6c9e053eb708fcfab8
Juile Harrow <julie.ann.harrow@gmail.com>
2016-08-01 09:27:55 -0400

    more practice

--- lesson_challenges/lesson03.js
+++ lesson_challenges/lesson03.js
@@ -1,1 +1,74 @@
 // Exercise 1 - Function Basics
+
+function gratuity(bill,gratPercent){
+  return bill*gratPercent/100;
+}
+
+function totalWithGrat(bill){
+  gratPercent = prompt("What percent gratuity does your server deserve?");
+  return bill+gratuity(bill,gratPercent)
+}
+
+bill = parseInt(prompt("How much is your bill?"))
+console.log("Your total including gratuity is $" + totalWithGrat(bill).toFixed(2));
+
+
+
+// Challenge 2 - Rock, Paper, Scissors
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+function getHand(){
+  var hands = ["rock","paper","scissors"];
+  return hands[parseInt(Math.random()*10%3)];
+}
+
+var numPlayers = Number(prompt("How many players?"));
+var players = [];
+
+for (var i = 1; i <= numPlayers; i++){
+  player = {
+    name: (prompt("Player "+ i +", what is your name?")).capitalizeFirstLetter(),
+    numWins:0,
+    hand:getHand()
+  }
+  players.push(player);
+}
+
+  while (numWins < 3) {
+    var userHand = (prompt("rock, paper, or scissors?")).toLowerCase();
+    var computerHand = options[parseInt(Math.random()*10%3)];
+    console.log("Computer played:  "  + computerHand);
+    console.log(name + ", you played:  "  + userHand);
+    if (computerHand === userHand) {
+      console.log("TIE!");
+    }
+    else if (computerHand === "rock" && userHand === "paper") {
+      console.log("You win!");
+      numWins += 1
+    }
+    else if (computerHand === "rock" && userHand === "scissors") {
+      console.log("You lose");
+    }
+    else if (computerHand === "paper" && userHand === "rock") {
+      console.log("You lose");
+    }
+    else if (computerHand === "paper" && userHand === "scissors") {
+      console.log("You win!");
+      numWins += 1
+    }
+    else if (computerHand === "scissors" && userHand === "paper") {
+      console.log("You lose");
+    }
+    else if (computerHand === "scissors" && userHand === "rock") {
+      console.log("You win!");
+      numWins += 1
+    }
+    else {
+      console.log("Please make sure you type 'rock', 'paper', or 'scissors' to play.")
+    }
+  }
+  console.log("That is 3 wins " + name + "! Congrats!")
+}
